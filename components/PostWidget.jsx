98c25614c6de1d10f745934ec06a8e781c9d7c68
Dreula <94791846+Dreula@users.noreply.github.com>
2022-08-11 13:55:03 +0800

    change icon

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,6 +1,9 @@
 import React, {useState, useEffect} from 'react'
 import moment from 'moment'
 import Link from 'next/link'
+import Image from 'next/image';
+
+import { grpahCMSImageLoader } from '../util';
 
 import { getRecentPosts, getSimilarPosts } from '../services'
 
@@ -18,19 +21,21 @@ const PostWidget = ({ categories, slug }) => {
   }, [slug]);
 
   return (
-    <div className='bg-white shadow-lg rounded-lg p-8 mb-8'>
+    <div className='bg-white shadow-lg rounded-lg p-8 pb-12 mb-8'>
         <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
           {slug ? 'Related Posts' : 'Recent Posts'}
         </h3>
         {relatedPosts.map((post) => (
           <div key={post.title} className='flex items-center w-full mb-4'>
             <div className='w-16 flex-none'>
-                <img
-                  alt={post.title}
-                  height="60px"
-                  width="60px"
-                  className="align-middle rounded-full"
-                  src={post.featuredImage.url}
+                <Image
+              loader={grpahCMSImageLoader}
+              alt={post.title}
+              height="60px"
+              width="60px"
+              unoptimized
+              className="align-middle rounded-full"
+              src={post.featuredImage.url}
                 />
             </div>
             <div className='flex-grow ml-4'>
